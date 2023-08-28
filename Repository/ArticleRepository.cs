086d5520aa4d4672586895e5d5c3dddec115dc2e
mariaruth1 <113031776+mariaruth1@users.noreply.github.com>
2023-08-28 14:12:28 +0200

    yes?

---
+++ Repository/ArticleRepository.cs
@@ -0,0 +1,78 @@
+﻿using Repository.Models;
+using Npgsql;
+using Dapper;
+
+namespace Repository;
+
+public class ArticleRepository
+{
+    private readonly NpgsqlDataSource _dataSource;
+    
+    public ArticleRepository(NpgsqlDataSource dataSource)
+    {
+        _dataSource = dataSource;
+    }
+    
+    public Article CreateArticle(CreateOrUpdateArticleRequestDto orUpdateArticle)
+    {
+        var sql = $@"
+INSERT INTO news.articles (headline, body, author, articleimgurl) 
+VALUES (@headline, @body, @author, @articleImgUrl) RETURNING *;
+";
+        using var conn = _dataSource.OpenConnection();
+        return conn.QuerySingle<Article>(sql, new {headline = orUpdateArticle.Headline, body = orUpdateArticle.Body, author = orUpdateArticle.Author, articleimgurl = orUpdateArticle.ArticleImgUrl});
+    }
+    
+    //The landing page should display a list of recent news stories.
+    //Each post should include a portion of the article text (body)
+    //- a maximum of 51 characters, the headline, the image and the ID.
+    //However, no author details should be present.
+    //LEFT takes the first 50 characters of the body text starting from the left.
+    public IEnumerable<NewsFeedItem> GetNewsFeed()
+    {
+        var sql = $@"
+SELECT articleid, headline, LEFT(body, 50) AS body, articleimgurl FROM news.articles";
+        using var conn = _dataSource.OpenConnection();
+        return conn.Query<NewsFeedItem>(sql);
+    }
+    
+    public Article GetArticle(int articleId)
+    {
+        var sql = $@"
+SELECT * FROM news.articles WHERE articleid = @articleId;
+";
+        using var conn = _dataSource.OpenConnection();
+        return conn.QuerySingle<Article>(sql, new { articleId });
+    }
+
+    public bool DeleteArticle(int articleId)
+    {
+        var sql = $@"
+DELETE FROM news.articles WHERE articleid = @articleId;
+";
+        using var conn = _dataSource.OpenConnection();
+        return conn.Execute(sql, new { articleId }) == 1;
+    }
+    
+    public Article UpdateArticle(CreateOrUpdateArticleRequestDto article, int articleId)
+    {
+        var sql = $@"UPDATE news.articles SET headline = @headline, body = @body, author = @author, articleimgurl = @articleimgurl 
+WHERE articleid = @articleId RETURNING *;
+";
+        using var conn = _dataSource.OpenConnection();
+        return conn.QuerySingle<Article>(sql, new { headline = article.Headline, body = article.Body, author = article.Author, articleimgurl = article.ArticleImgUrl, articleId });
+    }
+    
+    //Search for Articles: Users should be able to search for articles.
+    //The client will include the search term and page size in the query parameters.
+    //The search term should look for text matches in the body text.
+    //Results may not exceed the page size.
+    public IEnumerable<SearchArticleItem> SearchArticles(string searchTerm, int pageSize)
+    {
+        var sql = $@"
+SELECT * FROM news.articles WHERE body LIKE @searchTerm LIMIT @pageSize;
+";
+        using var conn = _dataSource.OpenConnection();
+        return conn.Query<SearchArticleItem>(sql, new { searchTerm = $"%{searchTerm}%", pageSize });
+    }
+}
---
+++ Services/ArticleService.cs
@@ -0,0 +1,101 @@
+﻿namespace Services;
+using Repository;
+using Repository.Models;
+
+public class ArticleService
+{
+    private readonly ArticleRepository _articleRepository;
+    private readonly DataValidator _validator;
+    
+    public ArticleService(ArticleRepository articleRepository, DataValidator dataValidator)
+    {
+        _articleRepository = articleRepository;
+        _validator = dataValidator;
+    }
+    
+    public Article CreateArticle(CreateOrUpdateArticleRequestDto article)
+    {
+        if (_validator.AuthorValidator(article))
+        {
+            try
+            {
+                return _articleRepository.CreateArticle(article);
+            }
+            catch (Exception)
+            {
+                throw new Exception("Could not create article");
+            }
+        }
+        throw new Exception("Author not valid");
+    }
+    
+    public IEnumerable<NewsFeedItem> GetNewsFeed()
+    {
+        try
+        {
+            return _articleRepository.GetNewsFeed();
+        }
+        catch (Exception)
+        {
+            throw new Exception("Could not get articles");
+        }
+    }
+    
+    public Article GetArticle(int articleId)
+    {
+        try
+        {
+            return _articleRepository.GetArticle(articleId);
+        }
+        catch (Exception)
+        {
+            throw new Exception("Could not get article");
+        }
+    }
+    
+    public bool DeleteArticle(int articleId)
+    {
+        try
+        {
+            return _articleRepository.DeleteArticle(articleId);
+        }
+        catch (Exception)
+        {
+            throw new Exception("Could not delete article");
+        }
+    }
+    
+    public Article UpdateArticle(CreateOrUpdateArticleRequestDto article, int articleId)
+    {
+        if (_validator.AuthorValidator(article))
+        {
+            try
+            {
+                return _articleRepository.UpdateArticle(article, articleId);
+            }
+            catch (Exception)
+            {
+                throw new Exception("Could not update article");
+            }
+        }
+
+        throw new Exception("Author not valid");
+    }
+    
+    public IEnumerable<SearchArticleItem> SearchArticles(string searchTerm, int pageSize)
+    {
+        if (searchTerm.Length > 2 && pageSize > 0)
+        {
+            try
+            {
+                return _articleRepository.SearchArticles(searchTerm, pageSize);
+            }
+            catch (Exception)
+            {
+                throw new Exception("Could not search articles");
+            }
+        }
+
+        throw new Exception("Search term must be at least 3 characters long");
+    }
+}
